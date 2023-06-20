f8d7b16ccf2a170aaf838201d1d4e8d9e8eee1c5
ariefnugraha <arief.nugraha@graveltechnology.com>
2023-06-20 17:10:34 +0700

    First commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Space_Grotesk } from 'next/font/google' 
 
-const inter = Inter({ subsets: ['latin'] })
+const space_grotesk = Space_Grotesk({ subsets: ['latin'], weight: ['400', '700'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={space_grotesk.className}>{children}</body>
     </html>
   )
 }
